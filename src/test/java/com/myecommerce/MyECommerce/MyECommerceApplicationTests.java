b47b9e67b53db5f82f8d8faf98fc0734dee28db5
geun02k <geun02k@gmail.com>
2025-01-17 19:47:02 +0900

    Initial commit

---
+++ src/test/java/com/myecommerce/MyECommerce/MyECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myecommerce.MyECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

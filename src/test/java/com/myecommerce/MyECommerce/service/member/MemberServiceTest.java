1755e44d7e0a5c06264d173953786e07bdb0fb56
geun02k <geun02k@gmail.com>
2025-02-02 08:00:04 +0900

    test : [003-SIGN-IN] 로그인 service 성공 테스트코드 작성

--- src/test/java/com/myecommerce/MyECommerce/service/member/MemberServiceTest.java
+++ src/test/java/com/myecommerce/MyECommerce/service/member/MemberServiceTest.java
@@ -16,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -112,10 +113,10 @@ class MemberServiceTest {
         given(passwordEncoder.encode(any()))
                 .willReturn("encode12345678");
 
-        // stub(가설) : 저장된 회원정보 Entity를 Dto로 변환 예상.
+        // stub(가설) : 저장된 회원정보 Dto를 Entity로 변환 예상.
         given(memberMapper.toEntity(any(MemberDto.class)))  // MemberDto -> Member 변환
                 .willReturn(expectMemberEntity);
-        // stub(가설) : 저장된 회원정보 Dto를 Entity로 변환 예상.
+        // stub(가설) : 저장된 회원정보 Entity를 Dto로 변환 예상.
         given(memberMapper.toDto(any(Member.class)))  // Member -> MemberDto 변환
                 .willReturn(expectMemberDto);
 
@@ -142,4 +143,72 @@ class MemberServiceTest {
         assertEquals('N', savedMember.getDelYn());
     }
 
+    @Test
+    @DisplayName("로그인성공")
+    void successSignIn() {
+        Long id = 1L;
+        String userId = "sky";
+        String password = "12345678";
+        String encodedPassword = "encode12345678";
+        String name = "김하늘";
+        String telephone = "01011112222";
+        String address = "서울 동작구 보라매로5가길 16 보라매아카데미타워 7층";
+        Character delYn = 'N';
+
+        // given
+        // 조회할 회원 DTO 객체 생성
+        MemberDto member = MemberDto.builder()
+                .userId(userId)
+                .password(password)
+                .build();
+        // 조회된 회원권한 Entity 객체 생성
+        List<MemberAuthority> expectRoleList =
+                Collections.singletonList(MemberAuthority.builder()
+                .id(id)
+                .authority(MemberAuthorityType.SELLER)
+                .build());
+
+        // stub(가설) : memberRepository.findByUserId() 실행 시 빈값 반환 예상.
+        given(memberRepository.findByUserId(any()))
+                .willReturn(Optional.ofNullable(Member.builder()
+                        .id(id)
+                        .userId(userId)
+                        .password(encodedPassword)
+                        .name(name)
+                        .telephone(telephone)
+                        .address(address)
+                        .delYn(delYn)
+                        .roles(expectRoleList)
+                        .build()));
+
+        // stub(가설) : passwordEncoder.matches() 실행 시 두 값이 일치하여 true 반환 예상.
+        given(passwordEncoder.matches(any(), any()))
+                .willReturn(true);
+
+        // stub(가설) : 저장된 회원정보 Entity를 Dto로 변환 예상.
+        given(memberMapper.toDto(any(Member.class)))  // Member -> MemberDto 변환
+                .willReturn(MemberDto.builder()
+                        .id(id)
+                        .userId(userId)
+                        .password(encodedPassword)
+                        .name(name)
+                        .telephone(telephone)
+                        .address(address)
+                        .delYn(delYn)
+                        .roles(expectRoleList)
+                        .build());
+
+        // when
+        MemberDto searchedMember = memberService.authenticateMember(member);
+
+        // then
+        assertEquals(id, searchedMember.getId());
+        assertEquals(userId, searchedMember.getUserId());
+        assertEquals(encodedPassword, searchedMember.getPassword());
+        assertEquals(name, searchedMember.getName());
+        assertEquals(telephone, searchedMember.getTelephone());
+        assertEquals(address, searchedMember.getAddress());
+        assertEquals(delYn, searchedMember.getDelYn());
+        assertEquals(expectRoleList.get(0).getAuthority(), searchedMember.getRoles().get(0).getAuthority());
+    }
 }

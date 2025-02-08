75acf8180fccd8306367f09e2d1ce9933daa739e
geun02k <geun02k@gmail.com>
2025-02-08 11:32:12 +0900

    feat : [007-SHOP-REGISTER] 상품, 옵션 Entity 생성

---
+++ src/main/java/com/myecommerce/MyECommerce/entity/production/Production.java
@@ -0,0 +1,49 @@
+package com.myecommerce.MyECommerce.entity.production;
+
+import com.myecommerce.MyECommerce.entity.BaseEntity;
+import com.myecommerce.MyECommerce.type.ProductionCategoryType;
+import com.myecommerce.MyECommerce.type.ProductionSaleStatusType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Production extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long seller;
+
+    @Column(nullable = false, length = 100)
+    private String code;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private ProductionCategoryType category;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private ProductionSaleStatusType saleStatus;
+
+    // 상품:옵션 (1:N)
+    // 상품의 옵션 데이터를 production_option 테이블 join해서 가져옴.
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "production")
+    private List<ProductionOption> options; // 한 상품이 여러 옵션을 가질 수 있음.
+
+}

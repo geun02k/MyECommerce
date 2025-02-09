ad7129f5535191196ea5c7937b219b23538f1bdc
geun02k <geun02k@gmail.com>
2025-02-09 09:32:31 +0900

    refactor : [007-SHOP-REGISTER] 상품등록 service의 상품등록, 상품옵션등록 로직 메서드로 분리

--- src/main/java/com/myecommerce/MyECommerce/service/production/ProductionService.java
+++ src/main/java/com/myecommerce/MyECommerce/service/production/ProductionService.java
@@ -1,6 +1,7 @@
 package com.myecommerce.MyECommerce.service.production;
 
 import com.myecommerce.MyECommerce.dto.production.RequestProductionDto;
+import com.myecommerce.MyECommerce.dto.production.RequestProductionOptionDto;
 import com.myecommerce.MyECommerce.dto.production.ResponseProductionDto;
 import com.myecommerce.MyECommerce.entity.member.Member;
 import com.myecommerce.MyECommerce.entity.production.Production;
@@ -34,61 +35,61 @@ public class ProductionService {
     @Transactional
     public ResponseProductionDto registerProduction(RequestProductionDto requestProductionDto,
                                                 Member member) {
-        // 판매자별 동일 상품코드 중복등록방지
-        productionRepository.findBySellerAndCode(member.getId(), requestProductionDto.getCode())
-                .ifPresent(existingProduction -> {
-                    throw new ProductionException(ALREADY_REGISTERED_CODE);
-                });
+        // 상품등록
+        Production savedProduction = saveProduction(requestProductionDto, member);
 
-        // 상품 dto -> entity 변환 및 상품등록 기본데이터 셋팅
-        Production production = this.setProductionEntityDataFromDto(
-                requestProductionDto, member);
-        // 상품옵션 dto -> entity 변환
-        List<ProductionOption> optionEntityList =
-                requestProductionDto.getOptions().stream()
-                        .map(productionOptionMapper::toEntity)
-                        .toList();
-
-        // 상품 등록
-        Production savedProduction = productionRepository.save(production);
-        // 상품옵션목록 등록
-        optionEntityList.forEach(option -> {
-            // JPA 연관관계를 위해 옵션에 상품객체 셋팅
-            option.setProduction(savedProduction);
-            // 상품코드 하위의 옵션코드 중복 체크
-            productionOptionRepository.findByProductionIdAndOptionCode(
-                    savedProduction.getId(), option.getOptionCode())
-                    .ifPresent(existingOption -> {
-                        throw new ProductionException(ALREADY_REGISTERED_OPTION_CODE);
-                    });
-            // 상품옵션등록
-            productionOptionRepository.save(option);
-        });
+        // 상품옵션등록
+        saveProductionOption(requestProductionDto.getOptions(), savedProduction);
 
         // 상품, 상품옵션목록 반환
         return productionMapper.toDto(savedProduction);
-
-//        // 추후 삭제 예정
-//        // 상품, 상품옵션목록 등록 및 반환
-//        // -> Production entity가 요청시 받은 options 정보까지도 가지고 함께 isnert 처리되기를 바람.
-//        // -> options가 production 정보를 가지고있지 않아서 productionId가 null로 입력됨.
-//        // -> 그래서 아래의 코드를 사용하지 못하고 상품과 상품옵션등록을 분할해서 진행.
-//        return productionMapper.toDto( // 상품, 상품옵션목록 반환
-//                productionRepository.save( // 상품, 상품옵션목록 등록
-//                        setProductionEntityDataFromDto(requestProductionDto, member))); // dto->entity변환 및 상품등록 기본데이터 셋팅
     }
 
-    // 상품등록을 위한 dto -> entity 변환 및 상품등록 기본데이터 셋팅
-    private Production setProductionEntityDataFromDto(RequestProductionDto productionDto,
-                                       Member member) {
-        Production productionEntity = productionMapper.toEntity(productionDto);
-
+    private Production saveProduction(RequestProductionDto requestProductionDto, Member member) {
+        // 상품 dto -> entity 변환
+        Production productionEntity = productionMapper.toEntity(requestProductionDto);
         productionEntity.setSeller(member.getId());
         productionEntity.setSaleStatus(ON_SALE);
         productionEntity.setOptions(null);
-        // production Entity 저장 시 option Entity를 자동저장하지 않도록 하기 위함인데
-        // 이렇게 null처리를 수동으로 해줘도 되는지는 모르겠음...
 
-        return productionEntity;
+        // 판매자별 상품코드 중복체크
+        checkIfProductionCodeExists(member.getId(), requestProductionDto.getCode());
+
+        // 상품 등록
+        return productionRepository.save(productionEntity);
+    }
+
+    private void saveProductionOption(List<RequestProductionOptionDto> options, Production savedProduction) {
+        // 상품옵션목록 dto -> entity 변환
+        List<ProductionOption> optionEntityList = options.stream()
+                .map(productionOptionMapper::toEntity)
+                .toList();
+
+        optionEntityList.forEach(optionEntity -> {
+            // 상품옵션목록의 JPA 연관관계를 위해 옵션에 상품객체 셋팅
+            optionEntity.setProduction(savedProduction);
+            // 상품옵션목록 중복체크
+            checkIfOptionCodeExists(optionEntityList);
+            // 상품옵션목록 등록
+            productionOptionRepository.save(optionEntity);
+        });
+    }
+
+    private void checkIfProductionCodeExists(Long sellerId, String code) {
+        productionRepository.findBySellerAndCode(sellerId, code)
+                .ifPresent(existingProduction -> {
+                    throw new ProductionException(ALREADY_REGISTERED_CODE);
+                });
+    }
+
+    private void checkIfOptionCodeExists(List<ProductionOption> optionList) {
+        // 상품코드 하위의 옵션코드 중복 체크
+        optionList.forEach(option -> {
+            productionOptionRepository.findByProductionIdAndOptionCode(
+                            option.getProduction().getId(), option.getOptionCode())
+                    .ifPresent(existingOption -> {
+                        throw new ProductionException(ALREADY_REGISTERED_OPTION_CODE);
+                    });
+        });
     }
 }

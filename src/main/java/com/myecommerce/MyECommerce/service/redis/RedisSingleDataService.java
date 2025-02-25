c295fa13a42c62738d64fd06c9f55d5236b973fc
geun02k <geun02k@gmail.com>
2025-02-26 08:15:55 +0900

    refactor : [012-CART-ADD] redis key, value 생성 메서드 추출해 분리

--- src/main/java/com/myecommerce/MyECommerce/service/redis/RedisSingleDataService.java
+++ src/main/java/com/myecommerce/MyECommerce/service/redis/RedisSingleDataService.java
@@ -15,22 +15,32 @@ public class RedisSingleDataService {
     private final RedisTemplate<String, Object> redisTemplate;
 
     /** Redis 단일 데이터 등록 **/
-    public void saveSingleData(RedisNamespaceType nameSpace, String key, Object value, Duration duration) {
-        String nameSpaceKey = nameSpace + ":" + key; // 네임스페이스를 포함한 키 생성
-        value = (value == null) ? ""  : value;
-        redisTemplate.opsForValue().set(nameSpaceKey, value, duration);
+    public void saveSingleData(RedisNamespaceType nameSpace,
+                               String key,
+                               Object value,
+                               Duration duration) {
+        redisTemplate.opsForValue()
+                .set(setKey(nameSpace, key), setObjectValue(value), duration);
     }
 
     /** Redis 단일 데이터 삭제&조회 **/
-    public Object getAndDeleteSingleData(RedisNamespaceType nameSpace, String key) {
-        String nameSpaceKey = nameSpace + ":" + key; // 네임스페이스를 포함한 키 생성
-        return redisTemplate.opsForValue().getAndDelete(nameSpaceKey);
+    public Object getAndDeleteSingleData(RedisNamespaceType nameSpace,
+                                         String key) {
+        return redisTemplate.opsForValue().getAndDelete(setKey(nameSpace, key));
     }
 
     /** Redis 단일 테이터 조회 **/
     public Object getSingleData(RedisNamespaceType nameSpace, String key) {
-        String nameSpaceKey = nameSpace + ":" + key; // 네임스페이스를 포함한 키 생성
-        return redisTemplate.opsForValue().get(nameSpaceKey);
+        return redisTemplate.opsForValue().get(setKey(nameSpace, key));
     }
 
+    // key값 셋팅 (네임스페이스를 포함한 키 생성)
+    private String setKey(RedisNamespaceType namespace, String key) {
+        return namespace + ":" + key;
+    }
+
+    // value값 셋팅
+    private Object setObjectValue(Object value) {
+        return (value == null) ? ""  : value;
+    }
 }

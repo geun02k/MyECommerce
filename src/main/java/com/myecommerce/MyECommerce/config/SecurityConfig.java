2cfffbccb489702fedfef53894629d9413f11800
geun02k <geun02k@gmail.com>
2026-01-08 13:25:18 +0900

    refactor : [015-REFACTOR-REQUEST-VALIDATION] /production 비조회 요청에 인증 요구하도록 변경
    production 경로의 보안 강화를 위해 GET 요청을 제외한 모든 요청에 대해 permitAll -> authenticated로 변경.

--- src/main/java/com/myecommerce/MyECommerce/config/SecurityConfig.java
+++ src/main/java/com/myecommerce/MyECommerce/config/SecurityConfig.java
@@ -51,8 +51,8 @@ public class SecurityConfig {
                         authorizeRequests
                             // 회원가입, 로그인 경로에 대해 모든인원 접근허용
                             .requestMatchers("/member/signup/**",
-                                              "/member/signin/**",
-                                              "/production").permitAll()
+                                             "/member/signin/**").permitAll()
+                            .requestMatchers(HttpMethod.GET,  "/production").permitAll()
                             .requestMatchers(HttpMethod.GET,  "/production/*").permitAll()
                             // 그 외 경로에 대해 인증필요
                             .anyRequest().authenticated())

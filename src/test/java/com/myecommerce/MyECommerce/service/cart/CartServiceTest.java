544b5c1b29d010e663101105a616263804f85c2d
geun02k <geun02k@gmail.com>
2026-01-23 23:43:33 +0900

    test: [020-REFACTOR-CART-ADD] 장바구니 상품추가 성공 Service 테스트 추가 (장바구니에 신규 상품 추가 성공)

--- src/test/java/com/myecommerce/MyECommerce/service/cart/CartServiceTest.java
+++ src/test/java/com/myecommerce/MyECommerce/service/cart/CartServiceTest.java
@@ -27,8 +27,7 @@ import static com.myecommerce.MyECommerce.type.RedisNamespaceType.CART;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CartServiceTest {
@@ -152,4 +151,73 @@ class CartServiceTest {
         assertEquals(2, responseCartDto.getQuantity());
     }
 
+    @Test
+    @DisplayName("장바구니추가 성공 - 장바구니에 요청 상품 미존재 시 상품 옵션 신규 추가")
+    void addCart_shouldAddNewItemWithRequestedQuantity_whenProductNotInCart() {
+        // given
+        // 요청 장바구니 상품 정보
+        RequestCartDto requestCartDto = RequestCartDto.builder()
+                .productCode("productCode")
+                .optionCode("optionCode")
+                .quantity(5)
+                .build();
+        // 요청 사용자 정보
+        Member member = member();
+
+        // DB 요청 상품옵션 정보 조회
+        RedisCartDto foundOptionDto = requestedItemNotInCart();
+
+        // Redis key
+        String redisKey = member.getUserId();
+        String redisHashKey = requestCartDto.getProductCode()
+                + ":" + requestCartDto.getOptionCode();
+
+        // 반환 상품정보
+        ResponseCartDto expectedResponseCartDto = ResponseCartDto.builder()
+                .productCode(foundOptionDto.getProductCode())
+                .optionCode(foundOptionDto.getOptionCode())
+                .price(foundOptionDto.getPrice())
+                .quantity(5)
+                .build();
+
+        // 요청 상품 Redis 장바구니에서 조회
+        given(redisSingleDataService.getSingleHashValueData(
+                eq(CART), eq(redisKey), eq(redisHashKey)))
+                .willReturn(null);
+        // 판매중인 상품옵션 DB에서 조회.
+        given(productOptionRepository.findByProductCodeAndOptionCodeOfOnSale(
+                eq(requestCartDto.getProductCode()),
+                eq(requestCartDto.getOptionCode())))
+                .willReturn(Optional.of(foundOptionDto));
+        // RedisCartDto -> 응답DTO 변환.
+        given(redisCartMapper.toResponseDto(any()))
+                .willReturn(expectedResponseCartDto);
+
+        // when
+        ResponseCartDto responseCartDto =
+                cartService.addCart(requestCartDto, member);
+
+        // then
+        // 정책 실행여부 검증
+        verify(cartPolicy, times(1))
+                .validateAdd(eq(requestCartDto.getProductCode()), eq(member));
+        // 요청 상품 장바구니 미존재해 상품 옵션 조회
+        verify(productOptionRepository, times(1))
+                .findByProductCodeAndOptionCodeOfOnSale(
+                        eq("productCode"), eq("optionCode"));
+        // redis 저장 실행여부 검증
+        ArgumentCaptor<RedisCartDto> redisCartDtoCaptor =
+                ArgumentCaptor.forClass(RedisCartDto.class);
+        verify(redisSingleDataService, times(1))
+                .saveSingleHashValueData(
+                        eq(CART), eq(redisKey), eq(redisHashKey),
+                        redisCartDtoCaptor.capture());
+        // 캡쳐 결과 검증
+        assertEquals(5, redisCartDtoCaptor.getValue().getQuantity());
+        // 반환 결과 검증
+        assertEquals(requestCartDto.getProductCode(), responseCartDto.getProductCode());
+        assertEquals(requestCartDto.getOptionCode(), responseCartDto.getOptionCode());
+        assertEquals(foundOptionDto.getPrice(), responseCartDto.getPrice());
+        assertEquals(5, responseCartDto.getQuantity());
+    }
 }

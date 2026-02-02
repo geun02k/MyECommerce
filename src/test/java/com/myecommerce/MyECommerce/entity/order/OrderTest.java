78143907938f7eb13ea6a14db270b013f6804b0b
geun02k <geun02k@gmail.com>
2026-02-03 01:22:47 +0900

    test: [022-ORDER-CREATE] 주문 Entity 실패 테스트 작성 (판매중이 아닌 상품 옵션 주문 불가)

--- src/test/java/com/myecommerce/MyECommerce/entity/order/OrderTest.java
+++ src/test/java/com/myecommerce/MyECommerce/entity/order/OrderTest.java
@@ -3,12 +3,14 @@ package com.myecommerce.MyECommerce.entity.order;
 import com.myecommerce.MyECommerce.entity.member.Member;
 import com.myecommerce.MyECommerce.entity.product.Product;
 import com.myecommerce.MyECommerce.entity.product.ProductOption;
+import com.myecommerce.MyECommerce.exception.ProductException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static com.myecommerce.MyECommerce.exception.errorcode.ProductErrorCode.PRODUCT_NOT_ON_SALE;
 import static com.myecommerce.MyECommerce.type.OrderStatusType.CREATED;
 import static com.myecommerce.MyECommerce.type.ProductSaleStatusType.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,6 +20,20 @@ class OrderTest {
     /* ------------------
         Test Fixtures
        ------------------ */
+
+    /** 판매중단된 주문 물품 생성 (유효하지 않은 주문 물품) */
+    OrderItem discontinuedItem() {
+        ProductOption registeredOption = ProductOption.builder()
+                .price(new BigDecimal("10000"))
+                .quantity(10)
+                .product(Product.builder()
+                        .saleStatus(DISCONTINUED)
+                        .build())
+                .build();
+        int orderQuantity = 3;
+
+        return OrderItem.createOrderItem(registeredOption, orderQuantity);
+    }
 
     /** 고객 생성 */
     Member member() {
@@ -64,6 +80,22 @@ class OrderTest {
 
         assertEquals(order, resultOrderItem.getOrder()); // 연관관계 검증
         assertEquals(new BigDecimal("30000"), order.getTotalPrice()); // 금액 계산 로직 검증
+    }
+
+    @Test
+    @DisplayName("주문 객체 생성 실패 - 판매 중이 아니면 주문 생성 불가")
+    void createOrder_shouldNotCreateOrder_whenProductNotOnSale() {
+        // given
+        // 주문 물품 목록
+        OrderItem discontinuedItem = discontinuedItem(); // 판매중단된 상품 옵션 주문
+        // 주문자
+        Member member = member();
+
+        // when
+        // then
+        ProductException e = assertThrows(ProductException.class, () ->
+                Order.createOrder(List.of(discontinuedItem), member));
+        assertEquals(PRODUCT_NOT_ON_SALE, e.getErrorCode());
     }
 
 }

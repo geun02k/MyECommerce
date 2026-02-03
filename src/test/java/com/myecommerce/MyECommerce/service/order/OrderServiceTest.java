1c88a9c525c031b0be338cf55403e2cd8592bfd8
geun02k <geun02k@gmail.com>
2026-02-04 07:15:21 +0900

    test: [022-ORDER-CREATE] 주문생성 Service 실패 테스트 생성 (정책 검증 실패)
    OrderService는 정책 검증 실패 시 어떤 부작용도 일으키지 않고 즉시 중단함을 검증.

--- src/test/java/com/myecommerce/MyECommerce/service/order/OrderServiceTest.java
+++ src/test/java/com/myecommerce/MyECommerce/service/order/OrderServiceTest.java
@@ -8,6 +8,7 @@ import com.myecommerce.MyECommerce.entity.order.Order;
 import com.myecommerce.MyECommerce.entity.order.OrderItem;
 import com.myecommerce.MyECommerce.entity.product.Product;
 import com.myecommerce.MyECommerce.entity.product.ProductOption;
+import com.myecommerce.MyECommerce.exception.OrderException;
 import com.myecommerce.MyECommerce.mapper.OrderMapper;
 import com.myecommerce.MyECommerce.repository.Order.OrderRepository;
 import com.myecommerce.MyECommerce.repository.product.ProductOptionRepository;
@@ -25,11 +26,11 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.myecommerce.MyECommerce.exception.errorcode.OrderErrorCode.ORDER_ITEM_MAX_QUANTITY_EXCEEDED;
 import static com.myecommerce.MyECommerce.type.MemberAuthorityType.CUSTOMER;
 import static com.myecommerce.MyECommerce.type.OrderStatusType.CREATED;
 import static com.myecommerce.MyECommerce.type.ProductSaleStatusType.ON_SALE;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
@@ -160,4 +161,36 @@ class OrderServiceTest {
         assertEquals(new BigDecimal("50000"), capturedOrderItem.getTotalPrice());
         assertEquals(registeredOption, capturedOrderItem.getOption());
     }
+
+    @Test
+    @DisplayName("주문생성 실패 - 주문생성 정책 검증 실패 시 어떤 상태 변경도 발생하지 않음.")
+    void createOrder_shouldThrowException_whenInvalidOrderRequest() {
+        // given
+        // 요청 고객
+        Member member = customer();
+        // 요청 주문물품
+        RequestOrderDto invalidRequestItem = RequestOrderDto.builder()
+                .productId(5L)
+                .optionCode("optionCode")
+                .quantity(51) // 주문 정책 제한: 물품 당 최대 주문 수량 초과
+                .build();
+
+        // 정책에서 예외 발생
+        doThrow(new OrderException(ORDER_ITEM_MAX_QUANTITY_EXCEEDED))
+                .when(orderPolicy)
+                .validateCreate(eq(List.of(invalidRequestItem)), eq(member));
+
+        // when
+        // then
+        // 정책이 왜 실패했는지가 아니라, 정책 실패 시 Service가 어떻게 반응하는지 검증.
+        // -> OrderService는 정책 검증 실패 시 어떤 부작용도 일으키지 않고 즉시 중단한다
+        OrderException e = assertThrows(OrderException.class, () ->
+                orderService.createOrder(List.of(invalidRequestItem), member));
+        verify(orderRepository, never()).save(any());
+        verify(stockCacheService, never()).decrementProductStock(any());
+        verify(orderMapper, never()).toResponseDto(any());
+        assertEquals(ORDER_ITEM_MAX_QUANTITY_EXCEEDED, e.getErrorCode());
+
+    }
+
 }

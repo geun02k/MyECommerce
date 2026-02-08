17f70bc1af54887145ceb4f9068799956e49b5cf
geun02k <geun02k@gmail.com>
2026-02-08 23:56:22 +0900

    feat: [023-PAYMENT-CREATE] 결제상태 도메인 의미 명확화 및 PG 요청 전 상태 READY 추가

--- src/main/java/com/myecommerce/MyECommerce/type/PaymentStatusType.java
+++ src/main/java/com/myecommerce/MyECommerce/type/PaymentStatusType.java
@@ -5,10 +5,11 @@ import lombok.AllArgsConstructor;
 // 결제상태 도메인
 @AllArgsConstructor
 public enum PaymentStatusType {
-    IN_PROGRESS("승인 대기 중"), // 결제 생성 직전 상태 (웹훅 응답 대기)
-    SUCCESS("승인 완료"),
-    FAILED("승인 실패"),
-    CANCELED("결제 취소")
+    READY("결제 생성 완료, PG 요청 전"),
+    IN_PROGRESS("PG 승인 대기 중"), // 웹훅 응답 대기
+    APPROVED("PG 승인 완료"),
+    FAILED("PG 승인 실패"),
+    CANCELED("PG 결제 취소")
     ;
 
     private final String description;

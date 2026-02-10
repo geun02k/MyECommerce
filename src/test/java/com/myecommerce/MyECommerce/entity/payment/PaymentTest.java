ab7dd65ab3bdb1617bdcd9206f48cbfa703acef3
geun02k <geun02k@gmail.com>
2026-02-10 22:37:40 +0900

    test: [023-PAYMENT-CREATE] 결제 Entity 실패 테스트 작성 (객체 생성 실패)
    주문상태가 CREATED(주문생성)가 아니면 객체 생성 불가.

--- src/test/java/com/myecommerce/MyECommerce/entity/payment/PaymentTest.java
+++ src/test/java/com/myecommerce/MyECommerce/entity/payment/PaymentTest.java
@@ -1,10 +1,13 @@
 package com.myecommerce.MyECommerce.entity.payment;
 
+import com.myecommerce.MyECommerce.dto.payment.PgApprovalResult;
+import com.myecommerce.MyECommerce.dto.payment.PgResult;
 import com.myecommerce.MyECommerce.entity.member.Member;
 import com.myecommerce.MyECommerce.entity.order.Order;
 import com.myecommerce.MyECommerce.entity.order.OrderItem;
 import com.myecommerce.MyECommerce.entity.product.Product;
 import com.myecommerce.MyECommerce.entity.product.ProductOption;
+import com.myecommerce.MyECommerce.exception.PaymentException;
 import com.myecommerce.MyECommerce.service.payment.PgClient;
 import com.myecommerce.MyECommerce.service.payment.TestPgClientImpl;
 import com.myecommerce.MyECommerce.type.PaymentMethodType;
@@ -15,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.List;
 
+import static com.myecommerce.MyECommerce.exception.errorcode.PaymentErrorCode.ORDER_STATUS_NOT_CREATED;
 import static com.myecommerce.MyECommerce.type.PaymentMethodType.CARD;
 import static com.myecommerce.MyECommerce.type.PaymentStatusType.*;
 import static com.myecommerce.MyECommerce.type.ProductSaleStatusType.ON_SALE;
@@ -64,6 +68,52 @@ class PaymentTest {
         return Order.createOrder(List.of(item), member);
     }
 
+    /** 결제 완료된 주문 생성 */
+    private Order paidOrder() {
+        // 주문 상품옵션
+        ProductOption productOption = productOption();
+        // 주문요청 상품옵션 수량
+        int orderQuantity = 3;
+        // 주문 물품 목록
+        OrderItem item = OrderItem.createOrderItem(productOption, orderQuantity);
+        // 주문자
+        Member member = member();
+
+        // 주문 생성
+        Order paidOrder = Order.createOrder(List.of(item), member);
+        // 주문에 대한 결제 생성
+        Payment payment = approvedPayment();
+        // 주문상태 결제완료로 변경
+        paidOrder.paid(payment);
+
+        return paidOrder;
+    }
+
+    /** PG 승인된 결제 생성 */
+    Payment approvedPayment() {
+        // 등록된 주문 (주문 상태는 CREATED)
+        Order order = order();
+        // 요청 결제 방식
+        PaymentMethodType requestMethod = CARD;
+        // 회사와 결제 계약된 PG사
+        PgProviderType pgProvider = pgProvider();
+
+        // 결제 생성 (READY)
+        Payment payment = Payment.createPayment(order, requestMethod, pgProvider);
+        // PG 결제 요청 (READY -> IN_)
+        PgResult pgResult = PgResult.builder()
+                .pgTransactionId("pgTransactionId")
+                .build();
+        payment.requestPgPayment(pgResult);
+        // PG 결제 승인
+        PgApprovalResult pgApprovalResult = PgApprovalResult.builder()
+                .approvalStatus(APPROVED)
+                .paidAmount(new BigDecimal("10000"))
+                .build();
+        payment.approve(pgApprovalResult);
+
+        return payment;
+    }
     /* ------------------
         Helper Method
        ------------------ */
@@ -93,6 +143,24 @@ class PaymentTest {
         assertEquals(requestMethod, payment.getPaymentMethod());
         assertEquals(pgProvider, payment.getPgProvider());
         assertEquals(READY, payment.getPaymentStatus()); // 결제 생성 상태
+    }
+
+    @Test
+    @DisplayName("결제 객체 생성 실패 - 주문상태가 CREATED(주문생성)가 아닌 경우 예외발생")
+    void createPayment_shouldNotCreatePayment_whenOrderStatusNotCreated() {
+        // given
+        // 등록된 주문 (주문 상태는 PAID)
+        Order invalidOrder = paidOrder();
+        // 요청 결제 방식
+        PaymentMethodType requestMethod = CARD;
+        // 회사와 결제 계약된 PG사
+        PgProviderType pgProvider = pgProvider();
+
+        // when
+        // then
+        PaymentException e = assertThrows(PaymentException.class, () ->
+                Payment.createPayment(invalidOrder, requestMethod, pgProvider));
+        assertEquals(ORDER_STATUS_NOT_CREATED, e.getErrorCode()); // 주문이 CREATED인 경우만 객체 생성 가능
     }
 
 }

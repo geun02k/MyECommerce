d2814bd7f54dbd6fed64644e2203a052c57d275f
geun02k <geun02k@gmail.com>
2026-07-24 00:51:17 +0900

    test: [027-FIX-PRODUCT-IDENTIFIER-DESIGN] 장바구니 상품추가 실패 통합테스트 작성 (동일 상품코드 조회 실패 문제 재현)
    기존 상품 조회 시, 동일한 productCode를 가진 두 상품 등록 후 상품을 조회하면 실패하는 시나리오를 통합테스트로 검증.

---
+++ src/test/java/com/myecommerce/MyECommerce/integration/cart/CartAddIntegrationTest.java
@@ -0,0 +1,142 @@
+package com.myecommerce.MyECommerce.integration.cart;
+
+import com.myecommerce.MyECommerce.dto.cart.RequestCartDto;
+import com.myecommerce.MyECommerce.dto.product.RequestProductDto;
+import com.myecommerce.MyECommerce.dto.product.RequestProductOptionDto;
+import com.myecommerce.MyECommerce.entity.member.Member;
+import com.myecommerce.MyECommerce.entity.member.MemberAuthority;
+import com.myecommerce.MyECommerce.integration.config.TestAuditingConfig;
+import com.myecommerce.MyECommerce.service.cart.CartService;
+import com.myecommerce.MyECommerce.service.product.ProductService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.myecommerce.MyECommerce.type.MemberAuthorityType.CUSTOMER;
+import static com.myecommerce.MyECommerce.type.ProductCategoryType.WOMEN_CLOTHING;
+import static com.myecommerce.MyECommerce.type.RedisNamespaceType.STOCK;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+@Import(TestAuditingConfig.class)
+public class CartAddIntegrationTest {
+
+    @Autowired
+    ProductService productService;
+
+    @Autowired
+    CartService cartService;
+
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
+    /* ------------------
+        Test Fixtures
+       ------------------ */
+
+    // 등록한 재고 캐시 키목록
+    List<String> stockCacheKeys = new ArrayList<>();
+
+    @AfterEach
+    void cleanUp() {
+        // 상품등록으로 인한 redis 재고 캐시 삭제
+        redisTemplate.delete(stockCacheKeys);
+    }
+
+    /** 셀러권한 사용자 */
+    Member seller(Long sellerId) {
+        return Member.builder()
+                .id(sellerId)
+                .userId("seller" + sellerId)
+                .build();
+    }
+
+    /** 고객권한 사용자 */
+    Member customer() {
+        return Member.builder()
+                .userId("customer")
+                .roles(List.of(MemberAuthority.builder()
+                        .authority(CUSTOMER)
+                        .build()))
+                .build();
+    }
+
+    /** 상품등록 */
+    void registerProduct(Long sellerId, String productCode, String optionCode) {
+        // 상품등록
+        RequestProductDto requestProduct = requestProductDto(productCode, optionCode);
+        Member seller = seller(sellerId);
+        productService.registerProduct(requestProduct, seller);
+
+        // redis 캐시 재고 삭제를 위한 키 생성
+        String stockKey = STOCK + ":" + productCode + ":" + optionCode;
+        stockCacheKeys.add(stockKey);
+    }
+
+    /** 상품등록 요청 상품 옵션 DTO 생성 */
+    RequestProductOptionDto requestOptionDto(String optionCode) {
+        return RequestProductOptionDto.builder()
+                .optionCode(optionCode)
+                .optionName("옵션명")
+                .price(new BigDecimal("10000"))
+                .quantity(5)
+                .build();
+    }
+
+    /** 상품등록 요청 상품 DTO 생성 */
+    RequestProductDto requestProductDto(String productCode, String optionCode) {
+        return RequestProductDto.builder()
+                .code(productCode)
+                .name("상품")
+                .category(WOMEN_CLOTHING)
+                .options(List.of(requestOptionDto(optionCode)))
+                .build();
+    }
+
+    /** 장바구니 상품추가 요청 DTO 생성 */
+    RequestCartDto requestCartDto(String productCode, String optionCode) {
+        return RequestCartDto.builder()
+                .productCode(productCode)
+                .optionCode(optionCode)
+                .quantity(1)
+                .build();
+    }
+
+    /* ------------------
+        장바구니 추가 Test
+       ------------------ */
+
+    // 기존 설계 문제 재현
+    @Test
+    @DisplayName("장바구니 추가 실패 - 동일한 상품코드가 존재하면 상품 단건 조회 실패")
+    void addCart_shouldFailedSearch_whenDuplicateProductCodeExists() {
+        // given
+        // 동일한 상품코드로 두 셀러가 상품 등록
+        registerProduct(1L, "productCode", "option1");
+        registerProduct(2L, "productCode", "option2");
+        // 장바구니 추가를 위한 객체 생성
+        Member member = customer();
+        RequestCartDto requestCartDto =
+                requestCartDto("productCode", "option2");
+
+        // when
+        // then
+        // productCode 단일 조회 결과가 2건이 되어 예외 발생
+        assertThrows(IncorrectResultSizeDataAccessException.class, () ->
+                cartService.addCart(requestCartDto, member));
+    }
+
+}
